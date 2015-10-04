787464a6d0bddfa0b02ead5c745790d658e6b543
Kseniya Khakalo <ksenyak16@gmail.com>
2015-10-04 23:12:08 +0300

    no message

---
+++ SE_01/1/src/main/java/logic/Logic.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class Logic {
+public String method(){
+	return "I am string in Logic.";}
+}
+
---
+++ SE_01/1/src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+import logic.Logic;
+public class Main {
+
+	public static void main(String[] args) {
+		Logic logic = new Logic();
+		System.out.println(logic.method());
+	}
+}
